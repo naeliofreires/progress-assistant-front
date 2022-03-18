19a0bed05b4fa57c8c8b8c2069fd972e9c439e70
Naélio Freires <naeliosistemas@gmail.com>
2022-03-18 15:42:15 -0300

    design: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,45 +1,23 @@
 import React from 'react';
+import logo from '/public/logo.svg';
+import task from '/public/task.svg';
 
 import * as S from './styles';
 import { useStore } from '/src/store/StoreProvider';
 
 export const Header = () => {
-  const store = useStore();
+  const amount = useStore().tasks.length;
 
   return (
     <S.Container>
-      <h1> ToDo </h1>
-      <button onClick={() => store.actions.reload()}>
-        <svg
-          enableBackground="new 0 0 50 50"
-          height="50px"
-          id="Layer_1"
-          version="1.1"
-          viewBox="0 0 50 50"
-          width="50px"
-          xmlSpace="preserve"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-        >
-          <rect fill="none" height="50" width="50" />
-          <polyline
-            fill="none"
-            points="40,7 40,16   31,15.999 "
-            stroke="#000000"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            strokeWidth="2"
-          />
-          <path
-            d="M41.999,25  c0,9.39-7.61,17-17,17s-17-7.61-17-17s7.61-17,17-17c5.011,0,9.516,2.167,12.627,5.616c0.618,0.686,1.182,1.423,1.683,2.203"
-            fill="none"
-            stroke="#000000"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            strokeWidth="2"
-          />
-        </svg>
-      </button>
+      <div>
+        <S.NextImage src={logo} width={100} height={100} />
+        <h1> Progress Escort </h1>
+      </div>
+      <div className="amount-task-view">
+        <span>{amount}</span>
+        <S.NextImage src={task} width={25} height={25} />
+      </div>
     </S.Container>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,23 +1,36 @@
 import styled from 'styled-components';
+import Image from 'next/image';
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 
-  button {
+  div {
     display: flex;
     align-items: center;
     justify-content: center;
+  }
 
-    margin-left: 16px;
-    border: 1px solid white;
-    border-radius: 10px;
-    background: #ededed;
+  div.amount-task-view {
+    display: none;
 
-    svg {
-      width: 25px;
-      height: 25px;
+    padding: 8px;
+    border-radius: 8px;
+    background: #e5efc1;
+    span {
+      font-weight: bold;
+      margin-right: 8px;
+    }
+  }
+
+  @media (min-width: 600px) {
+    justify-content: space-between;
+
+    div.amount-task-view {
+      display: flex;
     }
   }
 `;
+
+export const NextImage = styled(Image)``;
