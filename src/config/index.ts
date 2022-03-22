3009f27242b92faea153899f2a3eef6877b00a28
Naélio Freires <naeliosistemas@gmail.com>
2022-03-22 13:40:43 -0300

    update api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  // URL: 'https://to-do-list-api-nf.herokuapp.com/graphql',
-  URL: 'http://localhost:1337/graphql',
+  URL: 'https://to-do-list-api-nf.herokuapp.com/graphql',
+  // URL: 'http://localhost:1337/graphql',
 };
